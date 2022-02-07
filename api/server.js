d6d963d627fa3583616a083af64607bb5feeefb1
Brett Smith <brettsmith212@gmail.com>
2022-02-07 12:38:57 -1000

    server.get() working

--- api/server.js
+++ api/server.js
@@ -1,3 +1,43 @@
 // BUILD YOUR SERVER HERE
+const express = require("express");
+const server = express();
+const { find, findById } = require("./users/model");
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.json("hello world");
+});
+
+server.get("/api/users", (req, res) => {
+  find()
+    .then((user) => {
+      res.json(user);
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ message: "The users information could not be retrieved" });
+    });
+});
+
+server.get("/api/users/:id", (req, res) => {
+  let { id } = req.params;
+  findById(id)
+    .then((user) => {
+      if (user === null) {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist" });
+      } else {
+        res.json(user);
+      }
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ message: "The user information could not be retrieved" });
+    });
+});
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
